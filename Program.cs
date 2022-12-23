0723164c77e6eb5a567fcd3e674c18da839e96b3
alexfandomers <71307225+alexfandomers@users.noreply.github.com>
2022-12-23 16:09:27 -0300

    IntermediaryFields

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using IntermediaryFields;
+
+namespace IntermediaryFields
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {  
+        }
+    }
+    //a fieldcan be initialized in two ways: in a constructor or upon declaration
+    //the benefit of initializing a field during declaration is that if your class has
+    //one or more constructors, you'll make sure that the field will always be initialized
+    //irrespective of wich constructor is going to be called.
+     public class Customer
+    {
+        //public List <Order> Orders = new List<Order>();
+
+        //We use the readonly modifier to improve the robustness or our code
+        //when a field is declared with readonly, it needs to be initialized either during declaration
+        //or in a constructor. The value cannot be changed. This prevents you from accidentaly overwriting
+        //the value of a field, wich can result in an unexpected state. As an example, think of the orders
+        //in the above example. If we accidentally re-initialize this field somewhere else in the class,
+        //all the Order objects stored in the list will be lost. So we should declare it as readonly:
+
+        //public readonly List<Order> Orders = new List<Order>();
+    }
+}
